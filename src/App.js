f576692a1f1aa163fc1b6fc5abe28a7a4b65a957
wfercanas <fernando.canas@outlook.com>
2021-08-10 16:38:39 -0500

    add/ insert logo in app

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from "react";
 import { GlobalStyle } from "./GlobalStyles";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { ListOfPhotoCard } from "./components/ListOfPhotoCard";
+import { Logo } from "./components/Logo";
 
 const App = () => {
   return (
     <>
       <GlobalStyle />
+      <Logo />
       <ListOfCategories />
       <ListOfPhotoCard />
     </>
