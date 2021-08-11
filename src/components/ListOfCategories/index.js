85d3dee9c9bbac81c61f6dda3884afba1be41eb6
wfercanas <fernando.canas@outlook.com>
2021-08-10 19:26:09 -0500

    add/ list of categories minified and fixed on top

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,18 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-import { categories } from "../../../api/db.json";
 import { Category } from "../Category";
 import { StyledList, StyledListItem } from "./styles";
 
 const ListOfCategories = () => {
-  return (
-    <StyledList>
+  const [categories, setCategories] = useState([]);
+  useEffect(() => {
+    fetch("https://petgram-server-edsf8xpy2.now.sh/categories")
+      .then((response) => response.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  const onScroll = (event) => {
+    const newShowFixed = window.scrollY > 200;
+    if (showFixed !== newShowFixed) {
+      setShowFixed(newShowFixed);
+    }
+  };
+
+  const [showFixed, setShowFixed] = useState(false);
+  useEffect(() => {
+    document.addEventListener("scroll", onScroll);
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, [showFixed]);
+
+  const renderList = (fixed) => (
+    <StyledList className={fixed ? "fixed" : ""}>
       {categories.map((category) => (
         <StyledListItem key={category.id}>
           <Category {...category} />
         </StyledListItem>
       ))}
     </StyledList>
+  );
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   );
 };
 
