c23ceb02417fc083f5bbc4603aba4185a7234486
wfercanas <fernando.canas@outlook.com>
2021-08-11 18:22:47 -0500

    add/ render ListOfCard with render props

--- src/container/ListOfPhotoCard.js
+++ src/container/ListOfPhotoCard.js
@@ -1,6 +1,31 @@
-import { withPhotos } from "../hoc/withPhotos";
+import React from "react";
 import { ListOfPhotoCardComponent } from "../components/ListOfPhotoCard";
+import { gql } from "apollo-boost";
+import { Query } from "react-apollo";
 
-const ListOfPhotoCard = withPhotos(ListOfPhotoCardComponent);
+const query = gql`
+  query getPhotos($categoryId: ID) {
+    photos(categoryId: $categoryId) {
+      id
+      categoryId
+      src
+      likes
+      userId
+      liked
+    }
+  }
+`;
+
+const ListOfPhotoCard = ({ categoryId }) => {
+  return (
+    <Query query={query} variables={{ categoryId }}>
+      {({ loading, error, data = {} }) => {
+        const { photos = [] } = data;
+        console.log(photos);
+        return <ListOfPhotoCardComponent photos={photos} />;
+      }}
+    </Query>
+  );
+};
 
 export { ListOfPhotoCard };
