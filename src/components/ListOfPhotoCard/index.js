cdb871fa9807f48122270becf14cd87d935a1cf6
wfercanas <fernando.canas@outlook.com>
2021-08-11 16:09:01 -0500

    add/ export presentational component with props

--- src/components/ListOfPhotoCard/index.js
+++ src/components/ListOfPhotoCard/index.js
@@ -1,32 +1,14 @@
 import React from "react";
 import { PhotoCard } from "../PhotoCard";
 
-import { graphql } from "react-apollo";
-import { gql } from "apollo-boost";
-
-const withPhotos = graphql(gql`
-  query getPhotos {
-    photos {
-      id
-      categoryId
-      src
-      likes
-      userId
-      liked
-    }
-  }
-`);
-
 const ListOfPhotoCardComponent = ({ data: { photos = [] } } = {}) => {
   return (
     <ul>
-      {photos.map((photo, index) => (
+      {photos.map((photo) => (
         <PhotoCard key={photo.id} {...photo} />
       ))}
     </ul>
   );
 };
 
-const ListOfPhotoCard = withPhotos(ListOfPhotoCardComponent);
-
-export { ListOfPhotoCard };
+export { ListOfPhotoCardComponent };
