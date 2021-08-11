f2d39c218865726e49a61c1429821e64aced510c
wfercanas <fernando.canas@outlook.com>
2021-08-11 17:09:45 -0500

    add/ get detail data from urlparams

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
 import { ListOfCategories } from "./components/ListOfCategories";
 import { ListOfPhotoCard } from "./container/ListOfPhotoCard";
+import { PhotoCardWithQuery } from "./container/PhotoCardWithQuery";
 import { Logo } from "./components/Logo";
 
 const App = () => {
+  const urlParams = new window.URLSearchParams(window.location.search);
+  const detailId = urlParams.get("detail");
+
   return (
     <>
       <GlobalStyle />
       <Logo />
-      <ListOfCategories />
-      <ListOfPhotoCard categoryId={1} />
+      {detailId ? (
+        <PhotoCardWithQuery id={detailId} />
+      ) : (
+        <>
+          <ListOfCategories />
+          <ListOfPhotoCard categoryId={1} />
+        </>
+      )}
     </>
   );
 };
