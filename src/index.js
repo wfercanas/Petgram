447449e5e33c81bf3a799f4a235e98be142b01fd
wfercanas <fernando.canas@outlook.com>
2021-08-11 15:32:13 -0500

    add/ apolloClient and apolloProvider wrapping

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 
 import { App } from "./App";
+const client = new ApolloClient({
+  uri: "https://petgram-server-edsf8xpy2.now.sh/graphql",
+});
 
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("app")
+);
