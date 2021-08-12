c628487815f37ffd17cd9752f3cd0a7f62f4cb78
wfercanas <fernando.canas@outlook.com>
2021-08-12 15:42:18 -0500

    add/ basic layout for checking the page

---
+++ src/pages/Favs.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Favs = () => {
+  return <h1>Favs</h1>
+}
+
+export {Favs}
---
+++ src/pages/NotRegisteredUser.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NotRegisteredUser = () => {
+  return <h1>NotRegisteredUser</h1>;
+};
+
+export { NotRegisteredUser };
