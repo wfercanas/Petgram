4e2d6a1d6346be2ebfd8cb83b29c49cf29e1ce37
wfercanas <fernando.canas@outlook.com>
2021-08-12 15:42:38 -0500

    add/ routes for auth and not auth users

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import { GlobalStyle } from "./styles/GlobalStyles";
 import { Logo } from "./components/Logo";
 import { Home } from "./pages/Home";
 import { Detail } from "./pages/Detail";
+import { Favs } from "./pages/Favs";
+import { User } from "./pages/User";
+import { NotRegisteredUser } from "./pages/NotRegisteredUser";
 import { NavBar } from "./components/NavBar";
 
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: false });
+};
 
 const App = () => {
   return (
@@ -18,6 +25,21 @@ const App = () => {
           <Route path="/" exact component={Home} />
           <Route path="/pet/:categoryId" exact component={Home} />
           <Route path="/detail/:detailId" exact component={Detail} />
+          <UserLogged>
+            {({ isAuth }) =>
+              isAuth ? (
+                <>
+                  <Route path="/favs" exact component={Favs} />
+                  <Route path="/user" exact component={User} />
+                </>
+              ) : (
+                <>
+                  <Route path="/favs" component={NotRegisteredUser}></Route>
+                  <Route path="/user" component={NotRegisteredUser}></Route>
+                </>
+              )
+            }
+          </UserLogged>
         </Switch>
         <NavBar />
       </BrowserRouter>
