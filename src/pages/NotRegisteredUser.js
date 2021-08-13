0eac866977ac58cd94c311c3d1dbe3c4a322a804
wfercanas <fernando.canas@outlook.com>
2021-08-13 11:01:29 -0500

    add/ mutation for register user and login

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -1,15 +1,25 @@
 import React, { useContext } from "react";
 import { Context } from "../context/Context";
-import { UserForm } from "../components/UserForm.js";
+import { UserForm } from "../components/UserForm/index.js";
+import { RegisterMutation } from "../container/RegisterMutation";
 
 const NotRegisteredUser = () => {
   const { activateAuth } = useContext(Context);
   return (
     <>
-      <UserForm handleSubmit={activateAuth} title="Registrarse" />
+      <RegisterMutation>
+        {(register) => {
+          const onSubmit = ({ email, password }) => {
+            const input = { email, password };
+            const variables = { input };
+            register({ variables }).then(activateAuth);
+          };
+          return <UserForm onSubmit={onSubmit} title="Registrarse" />;
+        }}
+      </RegisterMutation>
       <UserForm handleSubmit={activateAuth} title="Iniciar Sesión" />
     </>
-  )
+  );
 };
 
 export { NotRegisteredUser };
