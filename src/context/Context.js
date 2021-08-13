7d30bb302facf413a189fefd55ea2712beae466d
wfercanas <fernando.canas@outlook.com>
2021-08-13 11:50:34 -0500

    add/ save token in session storage

--- src/context/Context.js
+++ src/context/Context.js
@@ -3,10 +3,13 @@ import React, { createContext, useState } from "react";
 const Context = createContext(null);
 
 const ContextProvider = ({ children }) => {
-  const [isAuth, setIsAuth] = useState(false);
+  const [isAuth, setIsAuth] = useState(() => {
+    return window.sessionStorage.getItem("token");
+  });
 
-  const activateAuth = () => {
+  const activateAuth = (token) => {
     setIsAuth(true);
+    window.sessionStorage.setItem("token", token);
   };
 
   const value = {
--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -13,7 +13,10 @@ const NotRegisteredUser = () => {
           const onSubmit = ({ email, password }) => {
             const input = { email, password };
             const variables = { input };
-            register({ variables }).then(activateAuth);
+            register({ variables }).then(({ data }) => {
+              const { signup } = data;
+              activateAuth(signup);
+            });
           };
 
           const errorMsg = error && "El usuario ya existe o hay algún problema";
@@ -33,7 +36,10 @@ const NotRegisteredUser = () => {
           const onSubmit = ({ email, password }) => {
             const input = { email, password };
             const variables = { input };
-            login({ variables }).then(activateAuth);
+            login({ variables }).then(({ data }) => {
+              const { login } = data;
+              activateAuth(login);
+            });
           };
 
           const errorMsg = error && "Credenciales incorrectas";
